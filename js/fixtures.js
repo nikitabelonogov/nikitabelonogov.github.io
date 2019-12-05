0a52f3e248159661de5482884d2c893d33c5349d
Nikita Belonogov <nikita.belonogov@kohls.com>
2019-12-05 12:15:09 +0400

    Add Yetanotherrougelike

--- js/fixtures.js
+++ js/fixtures.js
@@ -1,146 +1,160 @@
 projects = [
-  {
-    id: 'card-vega-power',
-    title: 'Vega Power',
-    text: '2D Platformer',
-    status: 'In Development',
-    image_url: './images/vega-power.png',
-    links: [
-      {
-        link: 'https://github.com/nikitabelonogov/VegaPower',
-        text: '<i class="fab fa-github"></i>',
-        disabled: true,
-      },
-      {
-        link: 'http://vegapower.s3-website-us-east-1.amazonaws.com',
-        text: 'Try out',
-      },
-    ]
-  },
-  {
-    id: 'card-catch-me-if-you-can',
-    title: 'Catch me if you can',
-    text: 'Catch all planes',
-    status: 'In Development',
-    image_url: './images/catch-me-if-you-can.png',
-    links: [
-      {
-        link: 'https://github.com/nikitabelonogov/CatchMeIfYouCan',
-        text: '<i class="fab fa-github"></i>',
-        disabled: true,
-      },
-      {
-        link: 'http://catchmeifyoucan.s3-website-us-east-1.amazonaws.com',
-        text: 'Try out',
-      },
-    ]
-  },
-  {
-    id: 'card-civilisationx',
-    title: 'CivilisationX',
-    text: 'Civilisation like game',
-    status: 'In Development',
-    image_url: './images/civilisationx.png',
-    links: [
-      {
-        link: 'https://github.com/nikitabelonogov/civilizationX',
-        text: '<i class="fab fa-github"></i>',
-        disabled: true,
-      },
-      {
-        link: 'http://civilisationx.s3-website-us-east-1.amazonaws.com',
-        text: 'Try out',
-      },
-    ]
-  },
-  {
-    id: 'card-telejenkins',
-    title: 'TeleJenkins',
-    text: 'This plugin allows Jenkins to send notifications via telegram bot.',
-    status: 'Released',
-    image_url: './images/telejenkins.svg',
-    links: [
-      {
-        link: 'https://github.com/jenkinsci/telegram-notifications-plugin',
-        text: '<i class="fab fa-github"></i>',
-      },
-      {
-        link: 'https://plugins.jenkins.io/telegram-notifications',
-        text: '<i class="fab fa-jenkins"></i>',
-      },
-    ]
-  },
-  {
-    id: 'card-party-parrot-status',
-    title: 'Party Parrot Status',
-    text: 'Providing a party parrot look to Jenkins Status indicators.',
-    status: 'Released',
-    image_url: './images/party-parrot-status.svg',
-    links: [
-      {
-        link: 'https://github.com/jenkinsci/partyparrotstatus-plugin',
-        text: '<i class="fab fa-github"></i>',
-      },
-      {
-        link: 'https://plugins.jenkins.io/partyparrotstatus',
-        text: '<i class="fab fa-jenkins"></i>',
-      },
-    ]
-  },
-  {
-    id: 'card-pidorbot',
-    title: 'PidorBot👹',
-    text: 'Just a nasty person @NAUGHTYPIDORBOT',
-    status: 'Released',
-    image_url: './images/pidorbot.svg',
-    links: [
-      {
-        link: 'https://github.com/co-code/pidor-bot',
-        text: '<i class="fab fa-github"></i>',
-      },
-      {
-        link: 'http://t.me/NAUGHTYPIDORBOT',
-        text: '<i class="fab fa-telegram"></i>',
-      },
-    ]
-  },
-  {
-    id: 'split-with-bot',
-    title: 'Split with Bot🍕',
-    text: 'Collaborative book of debtors @split_with_bot',
-    status: 'Released',
-    image_url: './images/split-with-bot.svg',
-    links: [
-      {
-        link: 'https://github.com/nikitabelonogov/split-with-bot',
-        text: '<i class="fab fa-github"></i>',
-      },
-      {
-        link: 'http://t.me/split_with_bot',
-        text: '<i class="fab fa-telegram"></i>',
-      },
-    ]
-  },
-  {
-    id: 'jewel',
-    title: 'Jewel💎',
-    text: 'Advert dashboard',
-    status: 'In Development',
-    links: [
-      {
-        link: 'https://github.com/ooow/jewel-f',
-        text: '<i class="fab fa-github"></i>',
-        tooltip: 'Private GitHub repository FrontEnd',
-      },
-      {
-        link: 'https://github.com/ooow/jewel-b',
-        text: '<i class="fab fa-github"></i>',
-        tooltip: 'Private GitHub repository BackEnd',
-      },
-      {
-        link: 'http://jewel-frontend.s3-website-eu-west-1.amazonaws.com',
-        text: '<i class="fas fa-link"></i>',
-      },
-    ]
-  },
+    {
+        id: 'yetanotherrougelike',
+        title: 'Yet another rougelike',
+        text: '',
+        status: 'In Development',
+        image_url: './images/yetanotherrougelike.png',
+        links: [
+            {
+                link: 'https://github.com/nikitabelonogov/YetAnotherRoguelike',
+                text: '<i class="fab fa-github"></i>',
+                disabled: true,
+            },
+        ]
+    },
+    {
+        id: 'card-vega-power',
+        title: 'Vega Power',
+        text: '2D Platformer',
+        status: 'In Development',
+        image_url: './images/vega-power.png',
+        links: [
+            {
+                link: 'https://github.com/nikitabelonogov/VegaPower',
+                text: '<i class="fab fa-github"></i>',
+                disabled: true,
+            },
+            {
+                link: 'http://vegapower.s3-website-us-east-1.amazonaws.com',
+                text: 'Try out',
+            },
+        ]
+    },
+    {
+        id: 'card-catch-me-if-you-can',
+        title: 'Catch me if you can',
+        text: 'Catch all planes',
+        status: 'In Development',
+        image_url: './images/catch-me-if-you-can.png',
+        links: [
+            {
+                link: 'https://github.com/nikitabelonogov/CatchMeIfYouCan',
+                text: '<i class="fab fa-github"></i>',
+                disabled: true,
+            },
+            {
+                link: 'http://catchmeifyoucan.s3-website-us-east-1.amazonaws.com',
+                text: 'Try out',
+            },
+        ]
+    },
+    {
+        id: 'card-civilisationx',
+        title: 'CivilisationX',
+        text: 'Civilisation like game',
+        status: 'In Development',
+        image_url: './images/civilisationx.png',
+        links: [
+            {
+                link: 'https://github.com/nikitabelonogov/civilizationX',
+                text: '<i class="fab fa-github"></i>',
+                disabled: true,
+            },
+            {
+                link: 'http://civilisationx.s3-website-us-east-1.amazonaws.com',
+                text: 'Try out',
+            },
+        ]
+    },
+    {
+        id: 'card-telejenkins',
+        title: 'TeleJenkins',
+        text: 'This plugin allows Jenkins to send notifications via telegram bot.',
+        status: 'Released',
+        image_url: './images/telejenkins.svg',
+        links: [
+            {
+                link: 'https://github.com/jenkinsci/telegram-notifications-plugin',
+                text: '<i class="fab fa-github"></i>',
+            },
+            {
+                link: 'https://plugins.jenkins.io/telegram-notifications',
+                text: '<i class="fab fa-jenkins"></i>',
+            },
+        ]
+    },
+    {
+        id: 'card-party-parrot-status',
+        title: 'Party Parrot Status',
+        text: 'Providing a party parrot look to Jenkins Status indicators.',
+        status: 'Released',
+        image_url: './images/party-parrot-status.svg',
+        links: [
+            {
+                link: 'https://github.com/jenkinsci/partyparrotstatus-plugin',
+                text: '<i class="fab fa-github"></i>',
+            },
+            {
+                link: 'https://plugins.jenkins.io/partyparrotstatus',
+                text: '<i class="fab fa-jenkins"></i>',
+            },
+        ]
+    },
+    {
+        id: 'card-pidorbot',
+        title: 'PidorBot👹',
+        text: 'Just a nasty person @NAUGHTYPIDORBOT',
+        status: 'Released',
+        image_url: './images/pidorbot.svg',
+        links: [
+            {
+                link: 'https://github.com/co-code/pidor-bot',
+                text: '<i class="fab fa-github"></i>',
+            },
+            {
+                link: 'http://t.me/NAUGHTYPIDORBOT',
+                text: '<i class="fab fa-telegram"></i>',
+            },
+        ]
+    },
+    {
+        id: 'split-with-bot',
+        title: 'Split with Bot🍕',
+        text: 'Collaborative book of debtors @split_with_bot',
+        status: 'Released',
+        image_url: './images/split-with-bot.svg',
+        links: [
+            {
+                link: 'https://github.com/nikitabelonogov/split-with-bot',
+                text: '<i class="fab fa-github"></i>',
+            },
+            {
+                link: 'http://t.me/split_with_bot',
+                text: '<i class="fab fa-telegram"></i>',
+            },
+        ]
+    },
+    {
+        id: 'jewel',
+        title: 'Jewel💎',
+        text: 'Advert dashboard',
+        status: 'In Development',
+        links: [
+            {
+                link: 'https://github.com/ooow/jewel-f',
+                text: '<i class="fab fa-github"></i>',
+                tooltip: 'Private GitHub repository FrontEnd',
+            },
+            {
+                link: 'https://github.com/ooow/jewel-b',
+                text: '<i class="fab fa-github"></i>',
+                tooltip: 'Private GitHub repository BackEnd',
+            },
+            {
+                link: 'http://jewel-frontend.s3-website-eu-west-1.amazonaws.com',
+                text: '<i class="fas fa-link"></i>',
+            },
+        ]
+    },
 ];
