eaa36f1061e786054e663f3196018e2991a1094b
Nikita Belonogov <waksaone@gmail.com>
2025-04-08 18:23:36 +0400

    upd

--- js/vue.js
+++ js/vue.js
@@ -1,47 +1,49 @@
 Vue.component('a-button-link', {
   props: ['text', 'link', 'disabled', 'tooltip'],
-  template: '' +
-    '<a v-if="link"' +
-    '   class="btn btn-primary btn-sm"' +
-    '   :class="{ \'disabled\': disabled }"' +
-    '   :href="link"' +
-    '   v-html="text"' +
-    '   data-toggle="tooltip"' +
-    '   data-placement="top"' +
-    '   :title="tooltip"></a>\n'
+  template: `
+    <a v-if="link"
+       class="btn btn-primary btn-sm"
+       :class="{ 'disabled': disabled }"
+       :href="link"
+       v-html="text"
+       data-toggle="tooltip"
+       data-placement="top"
+       :title="tooltip"></a>
+  `
 });
 
 Vue.component('project-card', {
   props: ['id', 'title', 'text', 'status', 'links', 'image', 'tags'],
-  template: '' +
-    '<div :id="id" class="m-2 card perspective-card" style="width: 20rem;">\n' +
-    '    <div v-if="image"' +
-    '         class="card-img-top bg-flex-fill" ' +
-    '         :style="{ \'background-image\': \'url(\' + image + \')\', height: \'5rem\' }">' +
-      '<span v-if="tags" v-for="tag in tags" class="badge badge-pill badge-primary" v-html="tag"></span>' +
-      '</div>\n' +
-    '    <div class="card-body">\n' +
-    '        <h4 class="card-title" v-html="title"></h4>\n' +
-    '        <p class="card-text" v-html="text"></p>\n' +
-    '    </div>\n' +
-    '    <div class="card-footer text-muted d-flex justify-content-between">\n' +
-    '        <span class="my-auto text-truncate" v-html="status"></span>\n' +
-    '        <div class="btn-group">\n' +
-    '            <a-button-link v-if="links" v-for="link in links"\n' +
-    '                           :key="link.link"\n' +
-    '                           :link="link.link"\n' +
-    '                           :text="link.text"\n' +
-    '                           :disabled="link.disabled"\n' +
-    '                           :tooltip="link.tooltip">\n' +
-    '            </a-button-link>\n' +
-    '        </div>\n' +
-    '    </div>\n' +
-    '</div>\n'
+  template: `
+    <div :id="id" class="m-2 card perspective-card" style="width: 20rem;">
+        <div v-if="image"
+             class="card-img-top bg-flex-fill"
+             :style="{ 'background-image': 'url(' + image + ')', height: '5rem' }">
+        </div>
+        <div class="card-body">
+            <span v-if="tags" v-for="tag in tags" class="badge rounded-pill text-bg-primary" v-html="tag"></span>
+            <h4 class="card-title" v-html="title"></h4>
+            <p class="card-text" v-html="text"></p>
+        </div>
+        <div class="card-footer text-muted d-flex justify-content-between">
+            <span class="my-auto text-truncate" v-html="status"></span>
+            <div class="btn-group">
+                <a-button-link v-if="links" v-for="link in links"
+                               :key="link.link"
+                               :link="link.link"
+                               :text="link.text"
+                               :disabled="link.disabled"
+                               :tooltip="link.tooltip">
+                </a-button-link>
+            </div>
+        </div>
+    </div>
+  `
 });
 
 new Vue({
   el: '#projects',
   data: {
-    projects : projects,
+    projects: projects,
   },
 });
